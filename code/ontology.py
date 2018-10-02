b90116f27adcd3b3dc3e13f9223480c3f1ab1d00
Ritwik Bose <mrmechko@gmail.com>
2018-10-02 18:07:43 -0400

    initial commit

---
+++ code/ontology.py
@@ -0,0 +1,15 @@
+import json
+
+
+def load_data(fname):
+    return json.load(open(fname))
+
+
+
+
+
+if __name__ == "__main__":
+    """This is how you load a json file, assuming you ran this from the base directory"""
+
+    ont = load_data("data/data.json")
+    print("there are %d nodes in the ontology" % len(ont))
---
+++ code/test.py
@@ -0,0 +1,28 @@
+"""
+The test file.  The grader will use the function defined in this file to test
+whether your system gives the right answer.  It should run with the following command
+`python -c "from code.test import test_file; test_file('inputfile', 'outputfile');"`
+"""
+
+
+def test_file(input_file, output_file):
+    """this function should load a test file and print the results to another
+    file.  The input file will contain a json list of semantic templates.
+    The output file should also be a json list of filled in semantic templates"""
+
+    print("This function needs to be implemented")
+    pass
+
+
+def subsumption_tests(input_file, output_file):
+    """This function should load a file containing pairs of ontology types and
+    output a file with either True or False on each line to indicate whether
+    the second type is an ancestor of the first type.  For example:
+    the input-file contains:
+    "person living-thing\n"
+
+    the output file should contain:
+    "True\n"
+    """
+    print("this function needs to be implemented")
+    pass
